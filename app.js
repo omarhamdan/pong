9707935c0a637cb36498b0ec57b89d814ecb813a
Omar Hamdan <omarhamdan1993@gmail.com>
2016-02-04 14:41:06 +0200

    border-fix + stop button

--- app.js
+++ app.js
@@ -17,34 +17,61 @@
       elem: ".ball"
     });
 
-    this.move();
+    this.start();
   }
 
   Pong.prototype.checkObstacles = function() {
 
-    if (((this.ball.x + this.ball.unitWidth) === this.unitWidth) || this.ball.x < 1) {
-      /* symmetry wrt x-axis */
-      this.ball.xDirection = this.ball.xDirection * -1;
-    } else if (((this.ball.y + this.ball.unitHeight) === this.unitHeight) || this.ball.y < 1) {
-      /* symmetry wrt y-axis */
-      this.ball.yDirection = this.ball.yDirection * -1;
+    let rightMost = this.ball.x + this.ball.unitWidth;
+    let topMost = this.ball.y + this.ball.unitHeight;
+
+    let topRightCorner = (rightMost === this.unitWidth) &&
+                         (topMost === this.unitHeight) &&
+                         this.ball.xDirection === 1 &&
+                         this.ball.yDirection === 1;
+
+    let topLeftCorner = (this.ball.x === 0) &&
+                        (topMost === this.unitHeight) &&
+                        this.ball.xDirection === -1 &&
+                        this.ball.yDirection === 1;
+
+    let bottomRightCorner = (rightMost === this.unitWidth) &&
+                            (this.ball.y === 0) &&
+                            this.ball.xDirection === 1 &&
+                            this.ball.yDirection === -1;
+
+    let bottomleftCorner = (this.ball.x === 0)  &&
+                           (this.ball.y === 0) &&
+                           this.ball.xDirection === -1 &&
+                           this.ball.yDirection === -1;
+
+    /* if corner */
+    if (topRightCorner || topLeftCorner || bottomRightCorner || bottomleftCorner) {
+      this.ball.xDirection = -this.ball.xDirection;
+      this.ball.yDirection = -this.ball.yDirection;
+    } else {
+      /* if border */
+      if ((rightMost >= this.unitWidth) || this.ball.x < 1) {
+        /* symmetry wrt x-axis */
+        this.ball.xDirection = -this.ball.xDirection;
+      } else if ((topMost >= this.unitHeight) || this.ball.y < 1) {
+        /* symmetry wrt y-axis */
+        this.ball.yDirection = -this.ball.yDirection;
+      }
     }
 
-    setTimeout(this.move.bind(this), 5);
+    setTimeout(this.move.bind(this), 0);
   }
 
-  Pong.prototype.move = function() {
-
-    this.ball.y = this.ball.y + (this.ball.yDirection * this.ball.yMagnitude * this.unit);
-    console.log(this.ball.y);
-    this.ball.x = this.ball.x + (this.ball.xDirection * this.ball.xMagnitude * this.unit);
-
-    this.ball.$elem.css({
-      bottom : (this.ball.y) + 'px',
-      left: (this.ball.x) + 'px'
+  Pong.prototype.start = function() {
+    let stop = true;
+    $('.stop-button').click(function() {
+      stop = false;
     });
-
-    this.checkObstacles();
+    while (!stop) {
+      this.ball.move();
+      this.ball.checkObstacles();
+    }
   }
 
   let Ball = function(options) {
@@ -58,14 +85,24 @@
     this.y = 0;
 
     /* Represent Direction */
-    this.xDirection = 1;
-    this.yDirection = 1;
+    this.xDirection = -1;
+    this.yDirection = -1;
 
     /* Represent Magnitude */
     this.xMagnitude = 1;
     this.yMagnitude = 1;
 
     this.$elem.width(this.unitWidth * this.unit).height(this.unitHeight * this.unit);
+  }
+
+  Ball.prototype.move = function() {
+    this.y = this.y + (this.yDirection * this.yMagnitude * this.unit);
+    this.x = this.x + (this.xDirection * this.xMagnitude * this.unit);
+
+    this.$elem.css({
+      bottom : (this.y) + 'px',
+      left: (this.x) + 'px'
+    });
   }
 
   $(function() {
